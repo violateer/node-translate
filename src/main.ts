ee3bbb5f8d481b06b7cf7b2ff5774126bf81ad04
violateer <1828257089@qq.com>
2020-12-01 18:57:04 +0800

    完成基本功能

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import * as https from 'https';
 import * as querystring from 'querystring';
 import * as md5 from 'md5';
+import { appId, appSecret } from './private';
 
 export const translate = (word) => {
-  const appId = '???';
-  const appSecret = '???';
   const salt = Math.random();
   const sign = md5(appId + word + salt + appSecret);
   const query: string = querystring.stringify({
@@ -15,22 +14,45 @@ export const translate = (word) => {
     salt: salt,
     sign: sign,
   });
+  type BaiduResult = {
+    from: string;
+    to: string;
+    trans_result: {
+      src: string;
+      dst: string;
+    }[];
+    error_code?: string;
+    error_msg?: string;
+  };
 
   const options = {
-    hostname: 'api.fanyi.baidu.com/',
+    hostname: 'api.fanyi.baidu.com',
     port: 443,
-    path: '/api/trans/vip/translate' + query,
+    path: '/api/trans/vip/translate?' + query,
     method: 'GET',
   };
 
-  const req = https.request(options, (res) => {
-    res.on('data', (d) => {
-      process.stdout.write(d);
+  const request = https.request(options, (response) => {
+    let chunks = [];
+    response.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
+    response.on('end', () => {
+      const string = Buffer.concat(chunks).toString();
+      const object: BaiduResult = JSON.parse(string);
+
+      if (object.error_code) {
+        console.log('ERROR:' + object.error_msg);
+        process.exit(2);
+      } else {
+        console.log(object.trans_result[0].dst);
+        process.exit(0);
+      }
     });
   });
 
-  req.on('error', (e) => {
+  request.on('error', (e) => {
     console.error(e);
   });
-  req.end();
+  request.end();
 };
